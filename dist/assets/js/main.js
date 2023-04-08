1457a7b11768a3de7ab38c2862a311526764877d
Anton9512 <Anton.mail9512@gmail.com>
2023-04-08 15:37:45 +0300

    add slider-dots

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1945,7 +1945,17 @@
 $(function () {
   $(".header__slider").slick({
     infinite: true,
-    arrows: true,
     fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="./assets/images/icons/arrows-left.svg" alt=""></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="./assets/images/icons/arrows-right.svg" alt=""></img>',
+    asNavFor: ".slider-dots",
+  });
+
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider",
   });
 });
