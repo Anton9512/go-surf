c3f807bafcd4108043d1b42e615d868cead1bd6b
Anton9512 <Anton.mail9512@gmail.com>
2023-04-14 12:07:41 +0300

    surf block

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1942,20 +1942,36 @@
     });
 });
 
-$(function () {
-  $(".header__slider").slick({
-    infinite: true,
-    fade: true,
-    prevArrow:
-      '<img class="slider-arrows slider-arrows__left" src="./assets/images/icons/arrows-left.svg" alt=""></img>',
-    nextArrow:
-      '<img class="slider-arrows slider-arrows__right" src="./assets/images/icons/arrows-right.svg" alt=""></img>',
-    asNavFor: ".slider-dots",
-  });
+$(".header__slider").slick({
+  infinite: true,
+  fade: true,
+  prevArrow:
+    '<img class="slider-arrows slider-arrows__left" src="./assets/images/icons/arrows-left.svg" alt=""></img>',
+  nextArrow:
+    '<img class="slider-arrows slider-arrows__right" src="./assets/images/icons/arrows-right.svg" alt=""></img>',
+  asNavFor: ".slider-dotshead",
+});
 
-  $(".slider-dots").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    asNavFor: ".header__slider",
-  });
+$(".slider-dotshead").slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  asNavFor: ".header__slider",
+});
+
+$(".surf-slider").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow:
+    '<img class="slider-arrows slider-arrows__left" src="./assets/images/icons/arrows-left.svg" alt=""></img>',
+  nextArrow:
+    '<img class="slider-arrows slider-arrows__right" src="./assets/images/icons/arrows-right.svg" alt=""></img>',
+  asNavFor: ".slider-map",
+});
+
+$(".slider-map").slick({
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".surf-slider",
+  focusOnSelect: true,
 });
